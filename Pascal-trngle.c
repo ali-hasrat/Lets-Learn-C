9a29b27c444cf53962b727a66b919b1a3d35af42
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2022-12-24 20:33:42 +0530

    Add files via upload

---
+++ Pascal-trngle.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+void main()
+{
+    int n,c=1,i,j,k;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        printf("  ");
+        for(k=0;k<=i;k++)
+        {
+            if (k==0||i==0)
+                c=1;
+            else
+               c=c*(i-k+1)/k;
+            printf("%4d",c);
+        }
+        printf("\n");
+    }
+}
