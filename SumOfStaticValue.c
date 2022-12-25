01f2c228882c8c481d9cb6a0109ac76a6f27ec86
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2022-12-26 01:34:48 +0530

    Add files via upload

---
+++ SumOfStaticValue.c
@@ -0,0 +1,30 @@
+//Method-1
+#include<stdio.h>
+void main()
+{
+    int a=4,b=2,c;
+    c=a+b;
+    printf("sum:%d",c);
+}  
+/*
+//Method-2
+
+#include<stdio.h>
+void main()
+{
+    if()
+    {
+
+    }
+}
+
+
+//Method-3
+
+#include<stdio.h>
+void main()
+{
+    switch(printf("Hello Sweety"))
+    {
+
+    }  */
