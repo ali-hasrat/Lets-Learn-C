05ee7c14f006afc786fa4c4095fdecf6bba71695
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2022-12-26 01:40:02 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,29 @@
+//Method-1
+#include<stdio.h>
+void main()
+{
+
+    printf("Hello Sweety");
+}  
+
+//Method-2
+
+#include<stdio.h>
+void main()
+{
+    if(printf("Hello Sweety"))
+    {
+
+    }
+}
+
+//Method-3
+
+#include<stdio.h>
+void main()
+{
+    switch(printf("Hello Sweety"))
+    {
+
+    }  
+}
