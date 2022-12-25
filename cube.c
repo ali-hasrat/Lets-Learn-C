4ee5e749a3b4789da144fd4ff2e07c5de4e991ba
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2022-12-26 01:47:01 +0530

    Update cube.c

--- cube.c
+++ cube.c
@@ -9,29 +9,3 @@ void main()
 }
 
 //& the same can also be solved through function
-
-
-
-
-
-
-
-
-/*
-//method-2
-#include<stdio.h>
-int calc(int a,int b)
-{
-    int calc();
-    return b;
-
-}
-void main()
-{
-    int a=3,b=2;
-    printf("sum:%d\n",a+b);
-    printf("sub:%d\n",a-b);
-    printf("mul:%d\n",a*b);
-    printf("div:%d\n",a/b);
-    printf("mod:%d",a%b); 
-}*/
