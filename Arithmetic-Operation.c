110858222a1839eac086b871b2943711023d2592
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2022-12-26 01:49:39 +0530

    Add files via upload

---
+++ Arithmetic-Operation.c
@@ -0,0 +1,31 @@
+/*#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int n,c;
+    scanf("%d",&n);
+    c=pow(n,3);
+    printf("cube:%d",c); 
+}
+
+//& the same can also be solved through function
+
+
+*/
+//method-2
+#include<stdio.h>
+int calc(int a,int b)
+{
+    int calc();
+    return b;
+
+}
+void main()
+{
+    int a=3,b=2;
+    printf("sum:%d\n",a+b);
+    printf("sub:%d\n",a-b);
+    printf("mul:%d\n",a*b);
+    printf("div:%d\n",a/b);
+    printf("mod:%d",a%b); 
+}
