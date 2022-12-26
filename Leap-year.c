af900d2c7281356ce08e291f91adc2b19209f73f
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2022-12-26 20:26:15 +0530

    Add files via upload

---
+++ Leap-year.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the year:");
+    scanf("%d",&n);
+    if((n%4==0) && ((n%400==0) || (n%100!=0)))
+    {
+        printf("%d is a leap year",n);
+    }
+    else
+    {
+      printf("\n%d is not a leap year",n);  
+    }
+}
+
