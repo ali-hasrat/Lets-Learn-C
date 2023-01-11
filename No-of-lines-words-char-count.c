4bf15096e69df4092ed3aae914163f66cd35e71f
Hasrat Ali <106477102+ali-hasrat@users.noreply.github.com>
2023-01-12 00:10:18 +0530

    Add files via upload

---
+++ No-of-lines-words-char-count.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[1000];
+    int i=0,a=1,b=1;
+    printf("Enter the String:");
+    gets(str);
+    while(str[i]!='\0')
+    {
+        if(str[i]==' ')
+        {
+        a++;
+        }
+          i++;
+
+        if(str[i]=='\n' || i==20)   // Here, i=10 means 1st line changed after 20 character
+        {
+            b++;
+        }
+
+    }
+    printf("Words count in String is=%d",a);
+    printf("\nTotal character in string=%d",strlen(str));
+    printf("\nLines count in String is=%d",b);
+    return 0;
+}
